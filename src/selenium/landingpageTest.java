5e82ebbe3022a2bd8c961c4d51780ccfd64ed67a
Heo Ik <heoi@ais-info.co.jp>
2025-08-23 00:04:33 +0900

    landigpageTest success

--- src/selenium/landingpageTest.java
+++ src/selenium/landingpageTest.java
@@ -1,8 +1,11 @@
 package selenium;
 
 import java.time.Duration;
+import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class landingpageTest {
@@ -10,15 +13,41 @@ public class landingpageTest {
 	public static void main(String[] args) throws InterruptedException {
 		
 		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
 		
 		//Data
-	
+		String FormAirport = "HBX";
+		String ToAirport = "CMB";
+		int DepartMonth = 4;
+		int DepartDay = 16;
+
 		
 		//연습사이트
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 		
+		//1st Step
+		driver.findElement(By.cssSelector("#ctl00_mainContent_ddl_originStation1_CTXTaction")).click();
+		driver.findElement(By.xpath("(//a[@value='"+FormAirport+"'])[1]")).click();
+		driver.findElement(By.xpath("(//a[@value='"+ToAirport+"'])[2]")).click();
+		driver.findElement(By.xpath("//td[@data-month='"+DepartMonth+"']/a[contains(.,'"+DepartDay+"')]")).click();
+		driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency")).click();
+		driver.findElement(By.cssSelector("option[value='AED']")).click();
+		driver.findElement(By.id("ctl00_mainContent_btn_FindFlights")).click();
+		System.out.println("First Step Passed");
 		
+		//2nd Step
+		driver.findElement(By.id("autosuggest")).sendKeys("ko");
+		List<WebElement> countrySelects = driver.findElements(By.cssSelector("a.ui-corner-all"));
+		for(WebElement countrySelect :countrySelects)
+		{
+			if(countrySelect.getText().equalsIgnoreCase("Korea, Republic of"))
+			{
+				countrySelect.click();
+				break;
+			}
+		}
+		System.out.println("Second Step Passed");
 		
 		
 		
