3c7e4590ddd051dbac74566bbd0429ed15ff8ee3
Heo Ik <heoi@ais-info.co.jp>
2025-09-18 22:25:19 +0900

    compelet

--- src/selenium/AlertTest.java
+++ src/selenium/AlertTest.java
@@ -1,9 +1,33 @@
 package selenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AlertTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+		// Input_AlertMSG
+		String userName = "AlertMSG";
+		driver.findElement(By.id("name")).sendKeys(userName);
+
+		String alertmessage = "Hello " + userName + ", share this practice page and share your knowledge";
+
+		// Alert
+		driver.findElement(By.id("alertbtn")).click();
+		String meesageCheck = driver.switchTo().alert().getText();
+
+		if (meesageCheck.equals(alertmessage)) {
+			driver.switchTo().alert().accept();
+			System.out.println("Pass");
+		} else {
+			driver.switchTo().alert().dismiss();
+			System.out.println("Fail");
+		}
 
 	}
 
